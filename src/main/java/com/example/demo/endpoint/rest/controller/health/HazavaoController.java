b83b3bedd789672769b04d7cffec5be610631b84
NomenaFitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-06-13 08:43:06 +0300

    feat: create /hazavao endpoint

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HazavaoController.java
@@ -0,0 +1,10 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+@RestController
+public class HazavaoController {
+
+  @GetMapping("/hazavao")
+  public String hazavao(){
+    return teny;
+  }
+}
